c043baa025f51f443e16320989bb4fdfe727bbe0
preston176 <prestonnyamweya@gmail.com>
2025-06-24 10:54:17 +0300

    Add "use client" directive to Header component and clean up Dashboard layout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Header from './_components/Header'
+import CreditsBar from './_components/CreditsBar'
 
 function Dashboard() {
     return (
         <div>
-            {/* Header */}
-          
-            {/* Sidebar */}
-
+            {/* Credits bar */}
+            <CreditsBar />
         </div>
     )
 }
