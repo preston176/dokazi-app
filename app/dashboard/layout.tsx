a0a2412f5cbe0e4450c5aa0c5808d7259e836260
preston176 <prestonnyamweya@gmail.com>
2025-06-24 11:49:41 +0300

    Remove Header component from layout and clean up code structure

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import Header from './_components/Header'
 
 function layout({ children }: { children: React.ReactNode }) {
     return (
         <div>
-            <Header />
             {children}
 
         </div>
