391120afb49da7acd7e0527d396e829fed1da32f
preston176 <prestonnyamweya@gmail.com>
2025-07-03 21:24:43 +0300

    feat: replace placeholder with CreateDocument component for editing functionality

--- app/(dashboard)/document/edit/[docId]/page.tsx
+++ app/(dashboard)/document/edit/[docId]/page.tsx
@@ -1,8 +1,9 @@
+import CreateDocument from '@/app/(dashboard)/dashboard/create-document/[docId]/page'
 import React from 'react'
 
 function Edit() {
     return (
-        <div>Edit</div>
+        <CreateDocument isEdit />
     )
 }
 
