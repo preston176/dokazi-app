a843ca6e830cff7c77b9226118d8caa58ed5e24b
preston176 <prestonnyamweya@gmail.com>
2025-07-03 21:24:56 +0300

    feat: create EditDocumentPage component to enable document editing functionality

---
+++ app/(dashboard)/document/edit/[docId]/preview/page.tsx
@@ -0,0 +1,10 @@
+import DocumentPreview from '@/app/(dashboard)/dashboard/create-document/[docId]/preview/page'
+import React from 'react'
+
+export default function EditDocumentPage() {
+    return (
+
+        <DocumentPreview isEdit />
+
+    )
+}
