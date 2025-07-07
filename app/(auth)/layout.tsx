3f9575d8da0a9b7a770172b70e31bdb68aab1d59
preston176 <prestonnyamweya@gmail.com>
2025-07-07 18:39:07 +0300

    feat: add layout component for consistent page structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function layout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className='flex items-center justify-center min-h-screen'>{children}</div>
+    )
+}
+
+export default layout
